0a7974cf32d512c9e5f437fd6b614fa4e1e2de7d
seema9797 <seemaprasad966@gmail.com>
2021-12-16 15:07:56 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var slider_img = document.querySelector('.slider-img');
+var images = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg'];
+var i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "images/"+images[i]);
+	
+}
